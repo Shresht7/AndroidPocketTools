bbd3f922f9ed1f18468886c5007af4047aad1f9d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-13 17:10:09 +0530

    chore: Optimize Imports

--- app/src/main/java/com/shresht7/pockettools/ui/screens/counter/CounterScreen.kt
+++ app/src/main/java/com/shresht7/pockettools/ui/screens/counter/CounterScreen.kt
@@ -17,14 +17,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Loop
 import androidx.compose.material.icons.filled.Remove
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
