088ec1d7acf3b0c8bd8f6537b61bfc1e39735f37
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-13 10:36:50 +0530

    feat: Update dependencies and add Kotlin Serialization
    Updates several core dependencies to their latest versions:
    - Kotlin: `2.0.21` -> `2.2.21`
    - Compose BOM: `2024.09.00` -> `2025.11.00`
    
    Introduces Kotlinx Serialization to the project by adding the necessary plugin and the `kotlinx-serialization-json` library.
    
    Also, the `compose-material-icons-extended` dependency is added to provide a wider range of icons.
    
    The Java and Kotlin JVM target compatibility is updated from version 11 to 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -30,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -58,4 +59,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.compose.material.icons.extended)
 }
