d8a31d2438458b4a679f72449aac03a9eec1783d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-15 13:21:29 +0530

    feat(magnetometer): Add preview for WaveformGraph
    Adds a `@Preview` composable for the `WaveformGraph` used in the Magnetometer screen.
    
    This preview, named `WaveformGraphPreview`, generates a list of 150 random float values to visualize the graph's appearance within Android Studio.

--- app/src/main/java/com/shresht7/pockettools/ui/screens/magnetometer/MagnetometerScreen.kt
+++ app/src/main/java/com/shresht7/pockettools/ui/screens/magnetometer/MagnetometerScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -108,4 +109,13 @@ fun WaveformGraph(values: List<Float>) {
             )
         }
     }
+}
+
+@Preview()
+@Composable
+fun WaveformGraphPreview() {
+    val values = List(150) {
+        (10..100).random().toFloat()
+    }
+    WaveformGraph(values)
 }
