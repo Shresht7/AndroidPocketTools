fdefa22eb3a0532b075573e30fdf1e0dbe034858
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-21 22:41:07 +0530

    feat(plumb bob): Add Plumb Bob tool using gravity sensor
    Introduces a new Plumb Bob screen that displays the device's tilt angle in degrees.
    
    - Creates `RotationVector.kt` with a `rememberUprightTilt` composable to read gravity sensor data and calculate the upright tilt.
    - Implements `PlumbBobScreen` with a UI that visually represents the angle using a rotating line and bob.
    - Displays the numerical tilt value at the top of the screen, formatted to one decimal place.

---
+++ app/src/main/java/com/shresht7/pockettools/data/RotationVector.kt
@@ -0,0 +1,50 @@
+package com.shresht7.pockettools.data
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import kotlin.math.abs
+import kotlin.math.acos
+import kotlin.math.atan2
+import kotlin.math.hypot
+import kotlin.math.sqrt
+
+
+@Composable
+fun rememberUprightTilt(): Float {
+    val context = LocalContext.current
+    val sensorManager = remember { context.getSystemService(Context.SENSOR_SERVICE) as SensorManager }
+    val gravitySensor = remember { sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) }
+
+    var tilt by remember { mutableFloatStateOf(0f) }
+
+    DisposableEffect(Unit) {
+        val listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                val gX = event.values[0]
+                val gY = event.values[1]
+//                val gZ = event.values[2]
+                tilt = Math.toDegrees(
+                    atan2(gX, abs(gY)).toDouble()
+                ).toFloat()
+
+            }
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+        }
+
+        sensorManager.registerListener(listener, gravitySensor, SensorManager.SENSOR_DELAY_UI)
+        onDispose { sensorManager.unregisterListener(listener) }
+    }
+
+    return tilt
+}
